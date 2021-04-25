ee2cc9dba095a26adc456d438d3617ceab89442c
Oskar Larsson <oskar.larsson@onewave.se>
2021-04-25 21:45:07 +0200

    Update fused_act.py

--- models/stylegan2/op/fused_act.py
+++ models/stylegan2/op/fused_act.py
@@ -80,4 +80,4 @@ class FusedLeakyReLU(nn.Module):
 
 
 def fused_leaky_relu(input, bias, negative_slope=0.2, scale=2 ** 0.5):
-    return F.leaky_relu(input, negative_slope=0.2)#FusedLeakyReLUFunction.apply(input, bias, negative_slope, scale)
+    return scale * F.leaky_relu(input + bias, negative_slope=0.2)#FusedLeakyReLUFunction.apply(input, bias, negative_slope, scale)

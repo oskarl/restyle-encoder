597caf9d7224fd56a355618c9c7d1c56581a2970
Oskar Larsson <oskar.larsson@onewave.se>
2021-04-25 21:36:10 +0200

    Update model.py

--- models/stylegan2/model.py
+++ models/stylegan2/model.py
@@ -146,7 +146,7 @@ class EqualLinear(nn.Module):
 
     def forward(self, input):
         if self.activation:
-            out = F.linear(input, self.weight * self.scale)
+            out = F.linear(input, self.weight * self.scale, bias=self.bias * self.lr_mul)
             out = fused_leaky_relu(out, self.bias * self.lr_mul)
 
         else:
